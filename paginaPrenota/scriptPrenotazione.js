ede1db8a1330b60f644bd891a480bf5f921a8c5a
tommasoerc <ercoli.1800047@studenti.uniroma1.it>
2020-06-01 17:07:35 +0200

    sistemato controllo data prenotazione

--- paginaPrenota/scriptPrenotazione.js
+++ paginaPrenota/scriptPrenotazione.js
@@ -1,16 +1,12 @@
 function validaFormPrenotazione(){
     var oggi = new Date();
-    var arr = document.formPrenotazione.data.value.split("-");
-    var d = new Date(arr[2], arr[1]-1, arr[0]);
-    if( d.getTime < oggi.getTime){
-        alert("Scegliere una data valida"); //NON FUNZIONA, BISOGNA RISOLVERE
+    var prenota = document.formPrenotazione.data.value; 
+
+    if( new Date(prenota).getTime() < oggi.getTime()){
+        alert("Scegliere data e ora non passate"); 
         return false;
     }
-    if (document.formPrenotazione.ora.value < 19: 30) { //NON FUNZIONA, BISOGNA RISOLVERE
-        alert("Il locale apre alle 19:30, non &egrave possibile prenotare prima di quell'ora");
-        document.formPrenotazione.ora.value = 19:30;
-        return false;
-    }
+
     if(document.formPrenotazione.numeroPosti.value < 1){
         alert("Prenotare almeno per una persona");
         return false;
