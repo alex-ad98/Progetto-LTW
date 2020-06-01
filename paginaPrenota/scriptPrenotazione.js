609bb57ed57f5657e33780c10d5226ae08f119f4
tommasoerc <ercoli.1800047@studenti.uniroma1.it>
2020-06-01 18:16:27 +0200

    controllo ora scipt

--- paginaPrenota/scriptPrenotazione.js
+++ paginaPrenota/scriptPrenotazione.js
@@ -1,11 +1,21 @@
 function validaFormPrenotazione(){
     var oggi = new Date();
+    //var data = new Date()
     var prenota = document.formPrenotazione.data.value; 
+    //var apertura = data.setHours(19,30,0);
+    //var chiusura = data.setHours(22);
 
     if( new Date(prenota).getTime() < oggi.getTime()){
         alert("Scegliere data e ora non passate"); 
         return false;
     }
+    var p = new Date(prenota)
+    if(p.getHours() < 20 || p.getHours() > 22){
+        alert("Inserire un orario compreso tra le 20:00 e le 22:00"); 
+        return false;
+    }
+
+    
 
     if(document.formPrenotazione.numeroPosti.value < 1){
         alert("Prenotare almeno per una persona");
